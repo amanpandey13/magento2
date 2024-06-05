8895625e07b20dc87244a36240e5f025a6e57c15
amanpandey13 <u.getaman1303@gmail.com>
2024-06-05 12:01:32 +0000

    Initial commit

---
+++ magento2/app/etc/env.php
@@ -0,0 +1,89 @@
+<?php
+return [
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'backend' => [
+        'frontName' => 'admin_3brcu41'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'SDtVVjjJiT8qTNODemFd7JeXdnn4kRiQ'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'f73_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'f73_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base64d5VPMRZhnwdL1biomXDCHq3gfZ0elJKo9wbkD9EsswI='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magentodb',
+                'username' => 'magentouser',
+                'password' => 'MyPassword',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'graphql_query_resolver_result' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        '13.232.236.171'
+    ],
+    'install' => [
+        'date' => 'Sun, 26 May 2024 13:25:08 +0000'
+    ]
+];
